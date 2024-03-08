92ec0fb98e4d989d1a7c5f06877e6e12a14b7b85
ameli vargas <andrisagi.16@gmail.com>
2024-03-08 16:06:39 -0500

    Fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CounterModule } from './Counter/counter.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes-module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AppComponent,
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
